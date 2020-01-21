095def16c118d8df7a2b4038a2334c0a70ffc518
Faisal Haque Bappy <hbfaisal66@gmail.com>
2020-01-21 07:57:30 +0600

    Updates

---
+++ precache-manifest.f81774fb3c6ed6e2f8029a676dbc25b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19d69499b3593031f6ab73769e6327e",
+    "url": "/sourcecode-blog/index.html"
+  },
+  {
+    "revision": "95f223ffd0e5999d45c5",
+    "url": "/sourcecode-blog/static/css/main.c623ac3b.chunk.css"
+  },
+  {
+    "revision": "4bd2e6d3a455d5e7c3db",
+    "url": "/sourcecode-blog/static/js/2.f14f39c8.chunk.js"
+  },
+  {
+    "revision": "f4c26eca1ec13091949231c30bb51905",
+    "url": "/sourcecode-blog/static/js/2.f14f39c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95f223ffd0e5999d45c5",
+    "url": "/sourcecode-blog/static/js/main.a344b1cb.chunk.js"
+  },
+  {
+    "revision": "28bf7d85f06a08c23d7f",
+    "url": "/sourcecode-blog/static/js/runtime-main.a4d051d3.js"
+  },
+  {
+    "revision": "305ee4a138b18648769b495023ceb17f",
+    "url": "/sourcecode-blog/static/media/Exo2-ExtraLight.305ee4a1.ttf"
+  },
+  {
+    "revision": "4ca744a6a9b4dfce169c1d013327b2a1",
+    "url": "/sourcecode-blog/static/media/Exo2-Light.4ca744a6.ttf"
+  },
+  {
+    "revision": "acc2415fccdea77368a301ecd6be3ce3",
+    "url": "/sourcecode-blog/static/media/Exo2-LightItalic.acc2415f.ttf"
+  },
+  {
+    "revision": "b9918945d1b49063e5a892f80f39b410",
+    "url": "/sourcecode-blog/static/media/Exo2-Regular.b9918945.ttf"
+  },
+  {
+    "revision": "7043377405d0968809c775d886c04fa3",
+    "url": "/sourcecode-blog/static/media/Exo2-SemiBold.70433774.ttf"
+  }
+]);
